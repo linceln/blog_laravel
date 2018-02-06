182b96622a0b960ee418a711da4638b6a95165b9
linceln <1>
2018-02-06 17:36:22 +0800

    Pagination changed

--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -12,8 +12,8 @@
 
   <nav aria-label="Page navigation example">
     <ul class="pagination">
-      <li class="page-item">
 
+      <li class="page-item">
         <a class="page-link" aria-label="Previous" href="{{ $posts->previousPageUrl() }}">
           <span aria-hidden="true">&laquo;</span>
           <span class="sr-only">Previous</span>
@@ -23,6 +23,13 @@
       @for ($i = 0; $i < $posts->lastPage(); $i++)
       <li class="page-item"><a class="page-link" href="/?page={{ $i + 1 }}{{ $param }}">{{ $i + 1  }}</a></li>
       @endfor
+
+      <li class="page-item">
+        <a class="page-link" aria-label="Next" href="{{ $posts->nextPageUrl() }}">
+          <span aria-hidden="true">&raquo;</span>
+          <span class="sr-only">Next</span>
+        </a>
+      </li>
 
     </ul>
   </nav>
