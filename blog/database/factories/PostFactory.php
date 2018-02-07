b5845f3a6dcc07db4458debb0ce80c373168ff96
linceln <1>
2018-02-07 13:39:10 +0800

    Set faker locale

--- blog/database/factories/PostFactory.php
+++ blog/database/factories/PostFactory.php
@@ -4,7 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Post::class,  function (Faker $faker) {
 	return [
-		'title' => $faker->sentence,
-		'body' => $faker->paragraph,
+		'title' => $faker->catchPhrase(),
+		'body' => $faker->realText($maxNbChars = 200, $indexSize = 2),
+		// 'body' => $faker->paragraph,
+		'created_at' => $faker->date($format = 'Y-m-d', $max = 'now'),
 	];
 });
